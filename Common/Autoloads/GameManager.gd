72dcef27b98981232ccdd77f7b2b811f246a732b
WyattBumbalough <149967386+WyattBumbalough@users.noreply.github.com>
2025-12-13 22:20:15 -0600

    WAVES
    + Got a wave system mostly working.
    + Made am enemy resource that gets loaded into a spawn pool. The pool gets shuffled, then we iterate through it and spawn an enemy of the respective type, and remove the resource from the pool.
    + When an enemy dies, checks to see if the size of the pool is greater than zero, and if it is, spawn the next enemy in the pool.

--- Common/Autoloads/GameManager.gd
+++ Common/Autoloads/GameManager.gd
@@ -5,6 +5,8 @@ signal wave_ended
 signal enemy_spawned # Emitted when a spawner spawns an enemy.
 signal enemy_killed # Emitted when an enemy's die function is called.
 
+var wave_in_progress: bool
+
 func _ready() -> void:
 	wave_started.connect(func(): print("Wave Started!"))
 	wave_ended.connect(func(): print("Wave ended!"))
--- Common/Spawning System/wave_manager.gd
+++ Common/Spawning System/wave_manager.gd
@@ -19,9 +19,6 @@ func _ready() -> void:
 	GameManager.enemy_killed.connect(_on_enemy_killed)
 	load_wave()
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("option"):
-		start_wave()
 
 func load_wave():
 	var wave = waves.front()
@@ -38,26 +35,6 @@ func load_wave():
 		e.type = Global.ENEMY_TYPES.HEAVY
 		spawn_pool.append(e)
 
-#func start_next_wave():
-	#if waves.is_empty():
-		#print("No more waves!")
-		#return
-	#
-	#GameManager.wave_started.emit()
-	#
-	#var wave: WaveResource = waves.front()
-	#var spawner_count = get_child_count() - 1
-	#
-	#for i in range(wave.light_enemies):
-		## Pick a random spawner node and spawn a light enemy.
-		#var random_index = rand.randi_range(0, spawner_count)
-		#var spawner: Spawner = get_child(random_index)
-		#spawner.spawn_entity(Refs.enemy_small)
-		#
-		#current_enemies += 1
-		#await get_tree().create_timer(spawn_delay).timeout
-		#
-	#waves.erase(wave)
 
 func start_wave():
 	spawn_pool.shuffle()
@@ -68,22 +45,21 @@ func start_wave():
 	current_enemies = total_spawns
 	
 	while spawn_amount > 0:
-		var e = spawn_pool.front()
-		var spawner = get_random_spawner()
-		spawn_enemy(e,spawner)
+		spawn_enemy()
 		spawn_amount -= 1
-		spawn_pool.erase(e)
 		await get_tree().create_timer(spawn_delay).timeout
 
 
-func spawn_enemy(_enemy: EnemyResource, spawner: Spawner):
+func spawn_enemy():
+	var _enemy = spawn_pool.front()
+	var _spawner = get_random_spawner()
 	if _enemy.type == Global.ENEMY_TYPES.LIGHT:
-		spawner.spawn_entity(Refs.enemy_small)
+		_spawner.spawn_entity(Refs.enemy_small)
 	elif _enemy.type == Global.ENEMY_TYPES.MEDIUM:
-		spawner.spawn_entity(Refs.enemy_medium)
+		_spawner.spawn_entity(Refs.enemy_medium)
 	else:
-		spawner.spawn_entity(Refs.enemy_heavy)
-
+		_spawner.spawn_entity(Refs.enemy_heavy)
+	spawn_pool.remove_at(0)
 
 func get_random_spawner():
 	var spawner_count = get_child_count() - 1
@@ -95,8 +71,16 @@ func _on_enemy_killed():
 	dead_enemies += 1
 	current_enemies -= 1
 	
-	if current_enemies > 0:
-		var e = spawn_pool.front()
-		var s = get_random_spawner()
-		spawn_enemy(e,s)
+	await get_tree().create_timer(0.5).timeout
 	
+	if spawn_pool.size() > 0:
+		spawn_enemy()
+	 
+	if current_enemies == 0:
+		GameManager.wave_ended.emit()
+
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("option"):
+		start_wave()
